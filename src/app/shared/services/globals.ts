9ac8cb451ba738ce73a722eafab534ae734ebcd7
Jitendra <jitendra@finoit.co.in>
2017-11-09 11:20:47 +0530

    change path

--- src/app/shared/services/globals.ts
+++ src/app/shared/services/globals.ts
@@ -1,4 +1,5 @@
 
 export class Global {
-    public static API_ENDPOINT = 'http://localhost:3005/api';
+    // public static API_ENDPOINT = 'http://localhost:3005/api';
+    public static API_ENDPOINT = 'http://34.215.244.39:3005/api';
 }
