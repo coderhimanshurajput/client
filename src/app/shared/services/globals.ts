6d9bdb1334e62710abdc28a29c39cd0be64b2ec9
Jitendra Kumar <jitendra@finoit.co.in>
2018-02-16 16:52:24 +0530

    changed server url

--- src/app/shared/services/globals.ts
+++ src/app/shared/services/globals.ts
@@ -1,6 +1,7 @@
 
 export class Global {
-    public static API_ENDPOINT = 'http://192.168.22.183:3005/api';
+    // public static API_ENDPOINT = 'http://172.20.10.3:3005/api';
+    // public static API_ENDPOINT = 'http://192.168.22.183:3005/api';
     // public static API_ENDPOINT = 'http://localhost:3005/api';
-    // public static API_ENDPOINT = 'http://34.215.244.39:3005/api';
+    public static API_ENDPOINT = 'http://34.215.244.39:3005/api';
 }
